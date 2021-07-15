2be001e47ed6cd72dd84aded961044d0a5fde4cb
deivit94 <continodavidnicolas@gmail.com>
2021-07-15 20:53:18 -0300

    Added calculator of Use of English

--- docs/app.js
+++ docs/app.js
@@ -161,3 +161,86 @@ function pressEnter(e){
         }
     }
 }
+
+/////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+function calcularValorDos() {
+    const n = document.getElementById('inputDos').value;
+    const scoreDos = document.getElementById('scoreDos')
+    const levelDos = document.getElementById('levelDos')
+
+    if (n == 28) {
+        scoreDos.innerHTML = '190'
+        levelDos.innerHTML = 'Level C1'
+    } else if (n == 27 || n == 26 || n == 25 ) {
+        scoreDos.innerHTML = Math.round(((n -24)*2.5) + 180);
+        levelDos.innerHTML = "Level C1";
+    } else if (n == 24) {
+        scoreDos.innerHTML = '180'
+        levelDos.innerHTML = 'Level C1'
+    } else if (n == 23 || n == 22 || n == 21 || n == 20 || n == 19) {
+        scoreDos.innerHTML = Math.round(((n - 18)* 3.333333333333333) + 160);
+        levelDos.innerHTML = "Level B2";
+    } else if (n == 18) {
+        scoreDos.innerHTML = '160'
+        levelDos.innerHTML = 'Level B2'
+    } else if (n == 17 || n == 16 || n == 15 || n == 14 || n == 13 || n == 12) {
+        scoreDos.innerHTML = Math.round(((n - 11)* 2.857142857142857) + 140);
+        levelDos.innerHTML = "Level B1";
+    } else if (n == 11) {
+        scoreDos.innerHTML = '140'
+        levelDos.innerHTML = 'Level B1'
+    } else if (n == 10 || n == 9 || n == 8 ) {
+        scoreDos.innerHTML = Math.round(((n - 7)* 4.5) + 122);
+        levelDos.innerHTML = "----";
+    } else if (n == 7 || n == 6 || n == 5 || n == 4 || n == 3 || n == 2 || n == 1) {
+        scoreDos.innerHTML = '122*';
+        levelDos.innerHTML = "----";
+    } else {
+        scoreDos.innerHTML = 'erorr';
+        levelDos.innerHTML = "error";
+    } 
+}
+
+//////////////////////////////////////////////////////////////
+
+function pressEnterDos(e) {
+    if (e.key == "Enter"){
+        const n = document.getElementById('inputDos').value;
+        const scoreDos = document.getElementById('scoreDos')
+        const levelDos = document.getElementById('levelDos')
+
+        if (n == 28) {
+            scoreDos.innerHTML = '190'
+            levelDos.innerHTML = 'Level C1'
+        } else if (n == 27 || n == 26 || n == 25 ) {
+            scoreDos.innerHTML = Math.round(((n -24)*2.5) + 180);
+            levelDos.innerHTML = "Level C1";
+        } else if (n == 24) {
+            scoreDos.innerHTML = '180'
+            levelDos.innerHTML = 'Level C1'
+        } else if (n == 23 || n == 22 || n == 21 || n == 20 || n == 19) {
+            scoreDos.innerHTML = Math.round(((n - 18)* 3.333333333333333) + 160);
+            levelDos.innerHTML = "Level B2";
+        } else if (n == 18) {
+            scoreDos.innerHTML = '160'
+            levelDos.innerHTML = 'Level B2'
+        } else if (n == 17 || n == 16 || n == 15 || n == 14 || n == 13 || n == 12) {
+            scoreDos.innerHTML = Math.round(((n - 11)* 2.857142857142857) + 140);
+            levelDos.innerHTML = "Level B1";
+        } else if (n == 11) {
+            scoreDos.innerHTML = '140'
+            levelDos.innerHTML = 'Level B1'
+        } else if (n == 10 || n == 9 || n == 8 ) {
+            scoreDos.innerHTML = Math.round(((n - 7)* 4.5) + 122);
+            levelDos.innerHTML = "----";
+        } else if (n == 7 || n == 6 || n == 5 || n == 4 || n == 3 || n == 2 || n == 1) {
+            scoreDos.innerHTML = '122*';
+            levelDos.innerHTML = "----";
+        } else {
+            scoreDos.innerHTML = 'erorr';
+            levelDos.innerHTML = "error";
+        } 
+    }
+}
